bee63303a7eb1f7e1d18f9bb739cbaf881dfb110
Aito <aaiittoo271@gmail.com>
2025-12-06 18:39:54 +0900

    clientがudp通信をできるようにした。

--- udp_server.py
+++ udp_server.py
@@ -1,4 +1,18 @@
 import socket
+
+rooms = {
+    "hoshina": {
+        "host_token": "DUMMY",
+        "host_ip": "127.0.0.1",
+        "members": {
+            "ksksksksksksksksk": {
+                "ip": "127.0.0.1",
+                "username": "test-user",
+            }
+        }
+    }
+}
+
 def main():
     sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0",5000))
@@ -6,6 +20,7 @@ def main():
 
     while True:
         data,address=sock.recvfrom(4096)
+        client_ip,client_port=address
         if len(data)<2:
             print("dataが短すぎます")
             continue
@@ -17,6 +32,32 @@ def main():
         print("roomname: ",roomname)
         print("token: ",token)
         print("message: ",message)
+
+        if roomname not in rooms:
+            print("そのルームは存在しません")
+            continue
+        room=rooms[roomname]
+        if token not in room["members"]:
+            print("tokenが間違ってます")
+            continue
+        member=room["members"][token]
+        if client_ip != member["ip"]:
+            print("ipアドレスが一致しません")
+            continue
+        print("認証完了：",member["username"],"からのメッセージは",message)
+        member["udp_addr"]=address
+        message_bytes=message.encode("utf-8")
+
+        for t,m in room["members"].items():
+            if "udp_addr" not in m:
+                continue
+            sock.sendto(message_bytes,m["udp_addr"])
+
+
+        
+
+if __name__=="__main__":
+    main()
 
 
 
